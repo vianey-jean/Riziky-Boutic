032a8a3f53ec6f074d0b42955239d4bba384c855
Jean Marie Vianey RABEMANALINA <vianey.jean@ymail.com>
2025-05-24 06:13:47 +0400

    Mise a jour correction des bugs

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,33 @@
 
+// Ajout du polyfill pour global
+window.global = window;
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { Toaster } from "./components/ui/sonner";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ThemeProvider } from './components/theme-provider'
 
-// Add global polyfill for browser use
-if (typeof global === 'undefined') {
-  window.global = window;
-}
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+          <App />
+          <Toaster />
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
