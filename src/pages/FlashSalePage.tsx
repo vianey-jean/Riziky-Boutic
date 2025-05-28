f9539fdbfa39b9faabe0675d3a3ad2f2295f90c1
Jean Marie Vianey RABEMANALINA <vianey.jean@ymail.com>
2025-05-28 08:28:46 +0400

    mettre a jour css

--- src/pages/FlashSalePage.tsx
+++ src/pages/FlashSalePage.tsx
@@ -235,8 +235,9 @@ const FlashSalePage: React.FC = () => {
         {/* Produits en vente flash */}
         <div className="mb-8">
           <h2 className="text-2xl font-bold text-gray-900 mb-6">
-            Produits en vente flash : {flashSaleProducts.length}
+              Produits en vente flash : <span className="text-red-800 font-bold">{flashSaleProducts.length}</span>
           </h2>
+
           
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {flashSaleProducts.map((product) => (
